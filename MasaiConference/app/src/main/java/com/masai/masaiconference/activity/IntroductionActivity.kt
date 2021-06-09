53e865e4003c40e0e76a712f220a7e2cc2dd8b2a
Nipun Jain <lucifernipun22@gmail.com>
2021-06-10 01:12:31 +0530

    "splashintroauthhomefrag"

---
+++ MasaiConference/app/src/main/java/com/masai/masaiconference/activity/IntroductionActivity.kt
@@ -0,0 +1,71 @@
+package com.masai.masaiconference.activity
+
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.github.appintro.AppIntro2
+import com.github.appintro.AppIntroFragment
+import com.github.appintro.AppIntroPageTransformerType
+
+import com.masai.masaiconference.R
+
+
+
+class IntroductionActivity : AppIntro2() {
+
+    companion object {
+        fun startActivity(context: Context) {
+            val intent = Intent(context, IntroductionActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        addIntroFragments()
+        setTransformer(AppIntroPageTransformerType.Flow)
+    }
+
+    override fun onSkipPressed(currentFragment: Fragment?) {
+        super.onSkipPressed(currentFragment)
+
+        //AppPref.isAppIntroShown = true
+        AuthenticationActivity.startActivity(this)
+        finish()
+    }
+
+    override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+
+       // AppPref.isAppIntroShown = true
+        AuthenticationActivity.startActivity(this)
+        finish()
+    }
+
+    private fun addIntroFragments() {
+        addSlide(
+            AppIntroFragment.newInstance(
+            getString(R.string.app_intro_meeting_title),
+            getString(R.string.app_intro_meeting_desc),
+            imageDrawable = R.drawable.img1,
+            backgroundDrawable = R.drawable.bg_app_intro
+        ))
+
+        addSlide(AppIntroFragment.newInstance(
+            getString(R.string.app_intro_chat_title),
+            getString(R.string.app_intro_chat_desc),
+            imageDrawable = R.drawable.img2,
+            backgroundDrawable = R.drawable.bg_app_intro
+        ))
+
+        addSlide(AppIntroFragment.newInstance(
+            getString(R.string.app_intro_meeting_history_title),
+            getString(R.string.app_intro_meeting_history_desc),
+            imageDrawable = R.drawable.ic_meeting_history,
+            backgroundDrawable = R.drawable.bg_app_intro
+        ))
+    }
+}
